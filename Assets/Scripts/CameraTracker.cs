079b962899dbbf723c3f7d736495b1d0f9590fe3
Jakobi Talbot <jakobi.talbot@gmail.com>
2020-05-19 22:42:39 +1000

    - Camera zooms out based on car speed
    - Simple track generation

--- Assets/Scripts/CameraTracker.cs
+++ Assets/Scripts/CameraTracker.cs
@@ -8,16 +8,27 @@ public class CameraTracker : MonoBehaviour
     private Transform m_target;
     [SerializeField]
     private Vector3 m_offset;
+    [SerializeField]
+    private float m_cameraLerpSpeed = 0.2f;
+    [SerializeField]
+    private float m_cameraSpeedDistancingCoefficient = 15f;
 
     private Transform m_transform;
+    private Vector3 m_lastTargetPosition;
 
     private void Start()
     {
         m_transform = transform;
+        m_lastTargetPosition = m_target.position;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        m_transform.position = m_target.position + m_offset;
+        // TODO: move camera down based on distance from ground (to see where landing)
+        Vector3 extraCameraDistance = -(Vector3.forward * (m_target.position - m_lastTargetPosition).magnitude * m_cameraSpeedDistancingCoefficient);
+        Vector3 newPos = m_target.position + m_offset + extraCameraDistance;
+        m_transform.position = Vector3.Lerp(m_transform.position, newPos, m_cameraLerpSpeed);
+
+        m_lastTargetPosition = m_target.position;
     }
 }
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_firstTrackPrefab;
+    [SerializeField]
+    private GameObject[] m_trackPrefabs;
+    [SerializeField]
+    private float m_trackPrefabLength = 25.25f;
+    [SerializeField]
+    private int m_levelLength = 10;
+
+    private void Start()
+    {
+        GenerateLevel();
+    }
+
+    private void GenerateLevel()
+    {
+        Instantiate(m_firstTrackPrefab, new Vector3(0, -10), Quaternion.identity);
+
+        for (int i = 1; i < m_levelLength; ++i)
+        {
+            Instantiate(m_trackPrefabs[Random.Range(0, m_trackPrefabs.Length)], new Vector3(m_trackPrefabLength * i, -10), Quaternion.identity);
+        }
+    }
+}
